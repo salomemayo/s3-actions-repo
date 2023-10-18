ea78ee0a7a1a76fa7126539b3ad782329fcf2925
salome mayowa <salomepadrai@yahoo.com>
2023-10-17 23:56:29 -0400

    backend

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
 module "s3" {
-  source = "git::git@github.com:salomemayo/s3-backend-repo.git//s3-module?ref=v1.1.0"
+  source = "git::git@github.com:salomemayo/s3-backend-repo.git//s3-module?ref=v1.2.0"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "salome-automation-salome"
+    region  = "us-west-2"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
 }
